973e57b5f2e1d5305f2645c7e560a09c7e1199a4
jaywomack <womackjw@hotmail.com>
2021-01-29 21:33:55 -0800

    Installed express and creating server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+const express = require('express');
+const app = express();
+const port = 3000;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-export default App;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
